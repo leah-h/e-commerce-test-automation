180009d9d9e07e953a9939947e80a85905c7d348
lh <harrichl@gmail.com>
2024-06-15 19:12:29 -0400

    add script for landing page

--- src/test/java/starter/navigation/PublixHomePage.java
+++ src/test/java/starter/navigation/PublixHomePage.java
@@ -1,12 +1,58 @@
 package starter.navigation;
 
 import net.serenitybdd.annotations.DefaultUrl;
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+
+import java.util.List;
 
 @DefaultUrl("https://www.publix.com/")
 public class PublixHomePage extends PageObject {
 
+    @FindBy(css= ".site-navigation")
+    private WebElementFacade siteNavigation;
+
+    @FindBy(css = ".navigation-search")
+    private WebElementFacade searchField;
+
+    @FindBy(id = "searchInputFlyoutMobile")
+    private WebElementFacade searchBox;
+
+    @FindBy(css = ".user-navigation .account")
+    private WebElementFacade userAccountNaivgation;
+
+    @FindBy(css = "[data-qa-automation='product-card-header']")
+    private WebElementFacade productCardHeader;
+
+    @FindBy(css = "[data-qa-automation='prod-title']")
+    private List<WebElementFacade> productTitles;
+
     public void navigatetoPublixPage() {
         open();
     }
+
+    public void nagivationShouldBeVisible() {
+        siteNavigation.shouldBeVisible();
+        searchField.shouldBeVisible();
+        userAccountNaivgation.shouldBeVisible();
+    }
+
+    public void searchFor(String keyword) {
+        searchBox.type(keyword);
+        searchBox.submit();
+    }
+
+    public boolean isFirstTitleContainsKeyword(String keyword) {
+        if (!productTitles.isEmpty()) {
+            String firstTitleText = productTitles.get(0).getText();
+            return firstTitleText.contains(keyword);
+        }
+        return false;
+    }
+
+    public void searchResultsPageIsDisplayed() {
+        productCardHeader.shouldBeVisible();
+    }
+
 }
--- src/test/java/starter/stepdefinitions/HomePageStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/HomePageStepDefinitions.java
@@ -1,14 +1,31 @@
 package starter.stepdefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import starter.navigation.PublixHomePage;
 
 public class HomePageStepDefinitions {
 
     PublixHomePage publixHomePage;
 
-    @Given("^user is on the hompage$")
-    public void user_is_on_the_home_page() {
+    @Given("^user is on the Publix landing page$")
+    public void navigateToHome() {
         publixHomePage.navigatetoPublixPage();
+    }
+
+    @Then("^the landing page should display correctly$")
+    public void landingPageIsDisplayed() {
+        publixHomePage.nagivationShouldBeVisible();
+    }
+
+    @When("^the user searches for (.*)$")
+    public void searchForKeyword(String keyword) {
+        publixHomePage.searchFor(keyword);
+    }
+
+    @When("^the search results page is displayed$")
+    public void searchResultsPageIsDisplayed() {
+        publixHomePage.searchResultsPageIsDisplayed();
     }
 }
